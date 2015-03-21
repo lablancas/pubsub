2cc189515aaee5aa80900f5b6bd506bac5d5c2ee
lablancas <lablancas@gmail.com>
2015-03-21 04:42:25 +0000

    first commit

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
     // Brief, one-line summary of the package.
     summary: 'A publish/subscribe package designed for Meteor',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/lablancas/pubsub',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
